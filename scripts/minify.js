ea59f7ec4b16e33b7388d538e47cc7538981993d
Jose <jfmdev@gmail.com>
2017-05-09 23:42:07 -0300

    Add minifier script

---
+++ scripts/minify.js
@@ -0,0 +1,20 @@
+// Load dependencies.
+const fs = require('fs');
+const UglifyJS = require('uglifyjs');
+const SaveLicense = require('uglify-save-license');
+
+// Minify code.
+let minified_code = UglifyJS.minify('source/ngComboDatePicker.js', {
+    output: {
+        comments: SaveLicense
+    }
+}).code;
+
+// Save result.
+fs.writeFile('source/ngComboDatePicker.min.js', minified_code, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was generated!");
+}); 
